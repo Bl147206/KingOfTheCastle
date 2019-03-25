53f192f9234d8e18664055639a03b42ee75a3d3a
Bl147206 <brady.lewis.206@k12.friscoisd.org>
2019-03-25 11:43:44 -0500

    j

--- KingOfTheCastle/KingOfTheCastle/Game1.cs
+++ KingOfTheCastle/KingOfTheCastle/Game1.cs
@@ -19,6 +19,8 @@ namespace KingOfTheCastle
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        int x = 0;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
