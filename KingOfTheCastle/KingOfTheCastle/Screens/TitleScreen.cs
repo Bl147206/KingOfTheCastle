c4982741f8b9c911fbac5fb3f872e9eb1458093d
alexFHS128 <alejandro.alonso.736@k12.friscoisd.org>
2019-03-27 11:33:42 -0500

    make dank screen thing

---
+++ KingOfTheCastle/KingOfTheCastle/Screens/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KingOfTheCastle {
+    class TitleScreen: Screen {
+        public void Update(GameTime gameTime) {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+
+        }
+    }
+}
