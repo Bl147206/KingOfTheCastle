f727610205dbb6e8706febbf936631014fb50ab1
Will Miller <46572166+Ceranth@users.noreply.github.com>
2019-03-31 13:14:22 -0500

    Platforms now do not intersect each other but still needs some bug fixes

--- KingOfTheCastle/KingOfTheCastle/Screens/StageScreen.cs
+++ KingOfTheCastle/KingOfTheCastle/Screens/StageScreen.cs
@@ -15,15 +15,33 @@ namespace KingOfTheCastle
     {
         Platform[] platforms;
         KeyboardState kb;
+        bool intersections;
         //Rectangle rect = new Rectangle(0, 0, 20, 20);
         public StageScreen()
         {
             platforms = new Platform[Globals.rng.Next(4)+3];
-
+            intersections = false;
             platforms[0] = new Platform(new Vector2(800,800), 800, 5);
             for(int x = 1; x<platforms.Length; x++)
             {
                 platforms[x] = new Platform(new Vector2((float)Globals.rng.Next(1600), (float)Globals.rng.Next(900)), Globals.rng.Next(100, 750), 5);
+                for(int y = 0;y<x; y++)
+                {
+                    if(platforms[x].destination.Intersects(platforms[y].destination))
+                        intersections = true;
+                }
+
+                while(intersections == true)
+                {
+                    intersections = false;
+                    platforms[x] = new Platform(new Vector2((float)Globals.rng.Next(1600), (float)Globals.rng.Next(900)), Globals.rng.Next(100, 750), 5);
+                    for (int y = 0; y < x; y++)
+                    {
+                        if (platforms[x].destination.Intersects(platforms[y].destination))
+                            intersections = true;
+                    }
+                    
+                }
             }
             
         }
