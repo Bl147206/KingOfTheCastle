dd862089569dd201ef78d602196551c941e18d97
Ceranth <will@stickpros.com>
2019-04-04 12:10:00 -0500

    Deleted all of my work

--- KingOfTheCastle/KingOfTheCastle/Screens/Shop.cs
+++ KingOfTheCastle/KingOfTheCastle/Screens/Shop.cs
@@ -9,6 +9,7 @@ namespace KingOfTheCastle
 {
     class Shop: Screen
     {
+        
         public override void Update(GameTime gameTime) {
 
         }
@@ -16,8 +17,7 @@ namespace KingOfTheCastle
         public override void Draw(GameTime gameTime)
         {
             game.GraphicsDevice.Clear(new Color(180,140,100));
-            game.spriteBatch.Draw(game.test, new Rectangle(Globals.screenW / 2, 0, 1, Globals.screenH),Color.Brown);
-            game.spriteBatch.Draw(game.test, new Rectangle(0, Globals.screenH / 2, Globals.screenW, 1), Color.Brown);
+
         }
     }
 }
