aac87dda26179aaa25205681bd9a13e15a129687
Bl147206 <brady.lewis.206@k12.friscoisd.org>
2019-04-04 11:41:11 -0500

    fixed title

--- KingOfTheCastle/KingOfTheCastle/Screens/TitleScreen.cs
+++ KingOfTheCastle/KingOfTheCastle/Screens/TitleScreen.cs
@@ -33,8 +33,8 @@ namespace KingOfTheCastle
         public TitleScreen(KingOfTheCastle game)
         {
             logo = game.Content.Load<Texture2D>("logo");
-            logoPos = new Rectangle(game.GraphicsDevice.Viewport.Width/2-400, game.GraphicsDevice.Viewport.Height/2-350, 800, 700);
-            textpos = new Vector2(game.GraphicsDevice.Viewport.Width/2-335, (float)(game.GraphicsDevice.Viewport.Height-game.GraphicsDevice.Viewport.Height/9));
+            logoPos = new Rectangle(Globals.screenW/2-400, Globals.screenH/2-350, 800, 700);
+            textpos = new Vector2(Globals.screenW/2-335, (float)(Globals.screenH-Globals.screenH/9));
             bg = new Color(255,0,0);
             currentColor = incColor.green;
         }
