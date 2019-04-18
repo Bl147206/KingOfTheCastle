351692d3708d8f903666832e550385c8083f9c8b
147206 <147206@S020RMD114-18>
2019-04-18 11:57:38 -0500

    quest

--- KingOfTheCastle/KingOfTheCastle/Quests/JumpQuest.cs
+++ KingOfTheCastle/KingOfTheCastle/Quests/JumpQuest.cs
@@ -13,6 +13,7 @@ namespace KingOfTheCastle
         Rectangle display;
         public JumpQuest(KingOfTheCastle game)
         {
+            this.game = game;
             requiredJumpAmt = game.round * Globals.rng.Next(3,10);
             title = "JUMP " + requiredJumpAmt + " TIMES";
             display = new Rectangle(Globals.screenW / 2 - 150, 0, 300, 200);
@@ -21,11 +22,12 @@ namespace KingOfTheCastle
         {
             foreach(Player p in game.players)
             {
-                if (p.jumps >= requiredJumpAmt && !p.completedMainQuest)
-                {
-                    p.completedMainQuest = true;
-                    p.gold += 10 + (5 * (game.round - 1));
-                }
+                if(p!=null)
+                    if (p.jumps >= requiredJumpAmt && !p.completedMainQuest)
+                    {
+                      p.completedMainQuest = true;
+                      p.gold += 10 + (5 * (game.round - 1));
+                    }
             }
         }
 
