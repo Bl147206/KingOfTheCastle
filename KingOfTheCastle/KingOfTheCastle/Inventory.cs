5b21600ae192a99509f3e0363ee661a2ff299362
alexFHS128 <alejandro.alonso.736@k12.friscoisd.org>
2019-05-03 11:50:38 -0500

    revert some balance stuff, will fix tomorrow

--- KingOfTheCastle/KingOfTheCastle/Inventory.cs
+++ KingOfTheCastle/KingOfTheCastle/Inventory.cs
@@ -7,8 +7,6 @@ namespace KingOfTheCastle {
     class Inventory {
         public Weapon[] weapons;
         int index = 0;
-        int attack;
-        double attackSpeed;
         public Inventory(int currentRound,KingOfTheCastle game) {
             this.weapons = new Weapon[3];
 
@@ -20,8 +18,8 @@ namespace KingOfTheCastle {
 
                 var kind = (Weapon.Kind)Globals.rng.Next(2);
 
-                    attack = Globals.rng.Next(currentRound * 3, currentRound*5);
-                    attackSpeed = (Globals.rng.NextDouble() + .2) * .4 + currentRound * .2;
+                int attack = Globals.rng.Next(currentRound * 2) + currentRound + 2;
+                double attackSpeed = (Globals.rng.NextDouble() + .2) * .4 + currentRound * .2;
 
                 switch (kind) {
                     case Weapon.Kind.melee:
