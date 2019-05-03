4491acceaefc6cb073ccf67562ca3e0fb5745612
147206 <147206@S020RMD114-18>
2019-05-03 11:40:10 -0500

    yeah i dont know

--- KingOfTheCastle/KingOfTheCastle/KingOfTheCastle.cs
+++ KingOfTheCastle/KingOfTheCastle/KingOfTheCastle.cs
@@ -36,6 +36,8 @@ namespace KingOfTheCastle
         public Texture2D[] backgrounds;
         public Texture2D swordAttackT;
         public SpriteFont playerFont;
+        public Texture2D arrow;
+        public Texture2D arrowF;
 
         public KingOfTheCastle()
         {
@@ -125,6 +127,8 @@ namespace KingOfTheCastle
             character = Content.Load<Texture2D>("Character");
             Coin = Content.Load<Texture2D>("Coin");
             swordAttackT = Content.Load<Texture2D>("swordSwipe");
+            arrow = Content.Load<Texture2D>("arrow");
+            arrowF = Content.Load<Texture2D>("arrowF");
 
             for(int x = 0; x<backgrounds.Length;x++)
             {
